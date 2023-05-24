a93e87bf4fe4e95eda4ddabf45880b4938d94dc6
Brendan Thompson <github@blt.is>
2023-05-24 21:38:41 +1000

    feat: add tasks, refactor lsp

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,93 +1,204 @@
-
-var langserver = null;
+var languageServer = null;
+var taskProvider = null;
 
 exports.activate = function() {
-    // Do work when the extension is activated
-    langserver = new TerraformLanguageServer();
+    languageServer = new TerraformLanguageServer();
+    taskProvider = new TerraformTaskProvider();
+
+
+    nova.assistants.registerTaskAssistant(taskProvider, {
+        "identifier": "terraform-nova",
+        "name": "Terraform Tasks"
+    });
 }
 
 exports.deactivate = function() {
-    // Clean up state before the extension is deactivated
-    if (langserver) {
-        langserver.deactivate();
-        langserver = null;
+    if (languageServer) {
+        languageServer.deactivate();
+        languageServer = null;
     }
 }
 
+class TerraformTaskProvider {
+    provideTasks() {
+        const terraformTask = new Task("Terraform")
+
+        terraformTask.setAction(
+            Task.Build,
+            new TaskProcessAction("/opt/homebrew/bin/terraform", {
+                args: ["plan"],
+                env: {},
+                cwd: nova.workspace.path,
+            })
+        )
+
+        terraformTask.setAction(
+            Task.Run,
+            new TaskProcessAction("", {
+                args: [],
+                env: {},
+            })
+        )
+
+        terraformTask.setAction(
+            Task.Clean,
+            new TaskProcessAction("", {
+                args: [],
+                env: {},
+            })
+        )
+
+
+        return [terraformTask]
+    }
+}
 
 class TerraformLanguageServer {
     constructor() {
         this.languageClient = null;
-        
-        nova.config.onDidChange('terraform-nova.language-server-path', (path) => {
-            this.start(path);
-        }, this);
-        
-        // Observe the configuration setting for the server's location, and restart the server on change
-        nova.config.observe('terraform-nova.language-server-path', function(path) {
-            this.start(path);
-        }, this);
-        
+        this.restartToken = null;
+        this.watcher = null;
+
+        nova.config.onDidChange("terraform-nova.language-server-path", (_path) => {
+            this.start();
+        }, this)
+
         this.start();
-        
-        let langserver = this;
+
+        nova.workspace.onDidChangePath((_path) => {
+            this.startWatcher();
+        }, this);
+
+        languageServer = this;
+
+        this.startWatcher();
     }
-    
+
     deactivate() {
         this.stop();
+
+        if (this.watcher) {
+            this.watcher.dispose()
+            this.watcher = null;
+        }
+
+        if (this.restartToken) {
+            clearTimeout(this.restartToken)
+        }
     }
-    
-    start(path) {
-        if (this.languageClient) {
-            this.languageClient.stop();
-            nova.subscriptions.remove(this.languageClient);
+
+    startWatcher() {
+        if (this.watcher) {
+            this.watcher.dispose()
+            this.watcher = null;
         }
-        
-        // Use the default server path
+
+        let workspacePath = nova.workspace.path;
+        if (workspacePath) {
+            this.watcher = nova.fs.watch("compile_commands.json", (path) => {
+                languageServer.fileDidChange(path);
+            });
+        }
+    }
+
+    start() {
+        console.log("Starting client")
+        notify("Starting Terraform language server.")
+
+        this.stop();
+
+        let path = nova.config.get("terraform-nova.language-server-path");
+        let args = ["serve"];
+
         if (!path) {
-            path = '/opt/homebrew/bin/terraform-ls';
+            path = "/opt/homebrew/bin/terraform-ls"
         }
-        
-        
-        
-        // Create the client
-        var serverOptions = {
+
+        let serverOptions = {
             path: path,
-            args: ['serve']
+            args: args,
         };
-        
-        var clientOptions = {
-            // The set of document syntaxes for which the server is valid
-            syntaxes: ['Terraform', 'tf', 'terraform']
+
+        let clientOptions = {
+            syntaxes: [
+                "terraform",
+                "Terraform",
+                "tf"
+            ]
         };
-        
-        var client = new LanguageClient('terraform-ls', 'Terraform Language Server', serverOptions, clientOptions);
-        
+
+        let client = new LanguageClient("terraform-ls", "Terraform Language Server", serverOptions, clientOptions);
+
+        client.onDidStop((error) => {
+            if (error) {
+                this.showStoppedUnexpectedlyNotification(error);
+            }
+        }, this);
+
         try {
-            console.log("Starting `terraform-ls` server at: '" + serverOptions.path + "' with: '" + serverOptions.args.join(" "))
-            
-            // Start the client
             client.start();
-            
-            // Add the client to the subscriptions to be cleaned up
+
             nova.subscriptions.add(client);
             this.languageClient = client;
-        }
-        catch (err) {
-            // If the .start() method throws, it's likely because the path to the language server is invalid
-            
-            if (nova.inDevMode()) {
-                console.error(err);
-            }
+        } catch (err) {
+            console.error(err);
         }
     }
-    
+
     stop() {
-        if (this.languageClient) {
-            this.languageClient.stop();
-            nova.subscriptions.remove(this.languageClient);
-            this.languageClient = null;
+        notify("Stopping the Terraform language server.")
+
+        let langclient = this.languageClient;
+        this.languageClient = null;
+
+        if (langclient) {
+            langclient.stop();
+            nova.subscriptions.remove(langclient);
         }
+    }
+
+    fileDidChange(path) {
+        let lastPathComponent = nova.path.basename(path);
+        if (lastPathComponent == "compile_commands.json" || lastPathComponent == "compile_flags.txt") {
+            this.scheduleRestart()
+        }
+    }
+
+    scheduleRestart() {
+        let token = this.restartToken;
+        if (token != null) {
+            clearTimeout(token);
+        }
+
+        let languageServer = this;
+        this.restartToken = setTimeout(() => {
+            languageServer.start();
+        }, 1000);
+    }
+
+    showStoppedUnexpectedlyNotification(error) {
+        let request = new NotificationRequest("com.brendanthompson.terraform-nova.terraform-ls.quit-unexpectedly");
+        request.title = "Terraform LS Quit Unexpectedly";
+        request.body = `The language server encountered an error: ${error}`;
+        request.actions = ["Restart", "Ignore"];
+
+        let languageServer = this;
+
+        let promise = nova.notifications.add(request);
+        promise.then(reply => {
+            if (reply.actionIdx == 0) {
+                // Restart server
+                languageServer.start();
+            }
+        });
     }
 }
 
+function notify(message) {
+    let request = new NotificationRequest("terraform")
+
+    request.title = "Terraform"
+    request.body = message
+
+    nova.notifications.add(request)
+}
